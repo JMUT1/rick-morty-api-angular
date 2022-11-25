e1c53ed7731064fe3abd08a60314b84c05d6153a
Chema Torres <chemaaa94@gmail.com>
2022-11-25 11:21:36 -0600

    changes routing and app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
 
   {
-  path: "",
-  redirectTo: '/home',
+  path: '',
+  redirectTo: 'home',
   pathMatch: 'full' },
 
 
-  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then((m)=>m.HomeModule) },
 
   { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
 
