96ffce65b06fdbc0107716bf78555e3ea118460a
Chema Torres <chemaaa94@gmail.com>
2022-12-22 08:35:44 -0600

    changes to the compoennts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "../app/components/pages/home/home.component"
+import {CharacterListComponent} from "../app/components/pages/characters/character-list/character-list.component"
+import {CharacterDetailsComponent} from "../app/components/pages/characters/character-details/character-details.component"
 
 const routes: Routes = [
 
@@ -9,11 +12,11 @@ const routes: Routes = [
   pathMatch: 'full' },
 
 
-  { path: 'home', loadChildren: () => import('./components/pages/home/home.module').then(m => m.HomeModule) },
+  { path: 'home',component: HomeComponent },
 
-  { path: 'character-list', loadChildren: () => import('./components/pages/characters/character-list/character-list.module').then(m => m.CharacterListModule) },
+  { path: 'character-list', component: CharacterListComponent},
 
-  { path: 'character-details/:id', loadChildren: () => import('./components/pages/characters/character-details/character-details.module').then(m => m.CharacterDetailsModule) }];
+  { path: 'character-details/:id', component:  CharacterDetailsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
+import {HomeComponent} from "../app/components/pages/home/home.component"
 import { FormSearchComponent } from './shared/components/form-search/form-search.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {CharacterListComponent} from "../app/components/pages/characters/character-list/character-list.component"
+import {CharacterDetailsComponent} from "../app/components/pages/characters/character-details/character-details.component"
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FormSearchComponent,
+    HomeComponent,
+    CharacterListComponent,
+    CharacterDetailsComponent,
 
 
 
